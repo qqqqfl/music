61c3b3e1b88669a76e315febaca6152c351173f6
qqqqfl <76077335+qqqqfl@users.noreply.github.com>
2022-01-01 14:31:19 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+      let UA = navigator.userAgent
+      console.log(UA.toLowerCase())
+      if (UA.toLowerCase().indexOf('mobile') >= 0) {
+        location.href = 'http://www.yuzh.top'
+      }
